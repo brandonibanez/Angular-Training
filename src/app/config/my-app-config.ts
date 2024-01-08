4c068565491df93cd1755b672adc113e01ba18fe
brandonibanez <ibanezbrandonn@gmail.com>
2024-01-08 20:14:36 +0800

    Login & Logout
    Uses Okta

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oaec41hprrgFKIJk5d7',
+        issuer: 'https://dev-37292218.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
