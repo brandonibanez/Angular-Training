e9e966ea0cc846b87a3b501bc0b0a90cdd76d43d
brandonibanez <ibanezbrandonn@gmail.com>
2024-01-11 20:52:13 +0800

    HTTPS Integration

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oaec41hprrgFKIJk5d7',
         issuer: 'https://dev-37292218.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 
